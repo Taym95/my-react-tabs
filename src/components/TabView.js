4c7726221599e584968b9212d54af04b391e91c2
Taym95 <haddadi.taym@gmail.com>
2018-10-08 09:48:46 +0200

    Add Tabview components with style

--- src/components/TabView.js
+++ src/components/TabView.js
@@ -1,13 +1,63 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import { isUndefined, has, isEmpty } from 'lodash'
+
+// components
+import TabViewNavigation from './TabViewNavigation';
+import TabViewContent from './TabViewContent';
+
+// helpers
+import { inheritParentProps, generateUniqueGuid } from '../helpers/helper'
 
 class TabView extends Component {
-    render() {
-        return (
-          <div>
 
+    constructor(props) {
+        super(props)
+
+        this.tabViewGuid = generateUniqueGuid();
+        this.state = {
+            currentTabIndex: (isUndefined(props.currentTabIndex) ? 0 : props.currentTabIndex)
+        }
+    }
+
+    handleTabClick = (tabIndex) => {
+        this.setState({
+            currentTabIndex: tabIndex
+        });
+
+    };
+
+    renderChildren() {
+        return React.Children.map(this.props.children, child => {
+            return inheritParentProps(this, child, ['id'], {
+                handleTabClick: this.handleTabClick,
+                currentTabIndex: this.state.currentTabIndex,
+                tabViewGuid: this.tabViewGuid
+            });
+        })
+    }
+
+    renderChildrenFromDataSource = () => {
+        return (
+          <React.Fragment>
+              <TabViewNavigation {...this.props} currentTabIndex={this.state.currentTabIndex} handleTabClick={this.handleTabClick} />
+              <TabViewContent {...this.props} currentTabIndex={this.state.currentTabIndex} />
+          </React.Fragment>
+        )
+    }
+
+    render() {
+
+        let className = 'tabview';
+        if (has(this.props.className) && !isEmpty(this.props.className))
+            className += ' ' + this.props.className;
+
+        const output = (has(this.props, 'dataSource') && !isUndefined(this.props.dataSource) ? this.renderChildrenFromDataSource() : this.renderChildren());
+
+        return (
+          <div id={this.tabViewGuid} className={className}>
+              {output}
           </div>
-        );
+        )
     }
 }
 
---
+++ src/helpers/helper.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { isNull, each, has, forOwn, isUndefined, isObject, indexOf, isEmpty } from 'lodash'
+
+export const inheritParentProps = (parent, child, propsToInherit, propsToAdd) => {
+
+    if (isNull(child))
+        return null;
+
+    let childProps = {};
+
+    // only add props when child is a object
+    if (isObject(child.type)) {
+
+        each(propsToInherit, (propName) => {
+
+            // only add child prop if parent exists
+            if (has(parent.props, propName)) {
+                // in case child prop has value this one is leading
+                childProps[propName] = (has(child.props, propName) ? child.props[propName] : parent.props[propName])
+            }
+
+        })
+
+        // add the following properties
+        if (!isUndefined(propsToAdd)) {
+            forOwn(propsToAdd, (value, key) => {
+                childProps[key] = value;
+            })
+        }
+
+    }
+
+    // when type is undefined the child is probably only a text value
+    if (isUndefined(child.type)) return child;
+    else return React.cloneElement(child, childProps);
+
+};
+
+export const generateUniqueGuid = () => {
+
+    const s4 = () => {
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+    };
+
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+
+};
